fb29e0b13ca4d46497b7ca8865138fd6c8f8f398
kakuta0915 <kakuta19960915@gmail.com>
2023-02-25 19:42:20 +0900

    ✨ 名前変更

--- libs/api.js
+++ libs/api.js
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk'
 
 export const client = createClient({
-  serviceDomain: 'kakuta-programming-blog',
+  serviceDomain: process.env.SERVICE_DOMAIM,
   apiKey: process.env.API_KEY,
 })
