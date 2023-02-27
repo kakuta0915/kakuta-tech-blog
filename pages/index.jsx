641adcd9a735a2b66087b34a8c9193b4fc4a5600
kakuta0915 <kakuta19960915@gmail.com>
2023-02-27 20:43:47 +0900

    ✨ TOPページに記事を表示する機能を追加

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
 import Meta from '@/src/components/meta/meta'
 import Container from '@/src/components/container/container'
 import Hero from '@/src/components/hero/hero'
+import { getAllPosts } from '@/libs/api'
+import Posts from '@/src/components/posts/posts'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <>
       <Container>
@@ -11,7 +13,19 @@ export default function Home() {
           pageDesc="プログラミング学習記録をまとめたサイト"
         />
         <Hero title="TOP" subtitle="トップページ" imageOn />
+
+        <Posts posts={posts} />
       </Container>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
