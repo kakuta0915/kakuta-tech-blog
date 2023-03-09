21051ad1cb56327322915597d28114e344226f56
kakuta0915 <kakuta19960915@gmail.com>
2023-03-09 19:59:54 +0900

    ✨ いったんコミット

--- libs/constants.js
+++ libs/constants.js
@@ -1,7 +1,7 @@
 export const siteMeta = {
   siteTitle: 'KAKUTA BLOG',
   siteDesc: 'アウトプットしていくサイト',
-  siteUrl: 'https://*************',
+  // siteUrl: 'https://',
   siteLang: 'ja',
   siteLocal: 'ja_JP',
   siteType: 'website',
