adbbe6ff382c69e64ef147364e304464e30448f7
kakuta0915 <kakuta19960915@gmail.com>
2023-05-16 19:45:22 +0900

    ✨ contact.jsxにformコンポーネントを追加した

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,6 +3,7 @@
 import Meta from '@/src/components/meta/meta'
 import Container from '@/src/components/container/container'
 import Hero from '@/src/components/hero/hero'
+import Form from '@/src/components/form/form'
 
 export default function Contact() {
   return (
@@ -14,6 +15,41 @@ export default function Contact() {
         />
         <Hero title="CONTACT" subtitle="お問い合わせ" imageOn />
       </Container>
+
+      <Form>
+        <form>
+          <ul>
+            <li>
+              <label for="username">
+                お名前
+                <span>【必須】</span>
+              </label>
+              <input type="text" id="username" name="username" />
+            </li>
+            <li>
+              <label for="company">会社名</label>
+              <input type="text" id="company" name="company" />
+            </li>
+            <li>
+              <label for="email">
+                メールアドレス
+                <span>【必須】</span>
+              </label>
+              <input type="email" id="email" name="email" />
+            </li>
+            <li>
+              <label for="detail">
+                お問い合わせ内容
+                <span>【必須】</span>
+              </label>
+              <textarea id="detail" name="detail"></textarea>
+            </li>
+          </ul>
+        </form>
+        <button type="submit" value="送信">
+          送信する
+        </button>
+      </Form>
     </>
   )
 }
---
+++ src/components/form/form.jsx
@@ -0,0 +1,7 @@
+// フォーム
+
+import styles from './form.module.css'
+
+export default function Form({ children }) {
+  return <div className={styles.form}>{children}</div>
+}
