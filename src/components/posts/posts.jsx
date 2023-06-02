0d3d390a34c84df71c509fb832675ecc4c475ba0
kakuta0915 <kakuta19960915@gmail.com>
2023-06-03 01:29:13 +0900

    ✨ 記事一覧に日付とカテゴリーを追加した

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -3,13 +3,16 @@
 import styles from './posts.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTag } from '@fortawesome/free-solid-svg-icons'
+import ConvertDate from '../convert/convert-date'
 
 export default function Posts({ posts }) {
   return (
-    <div className={styles.gridContainer}>
-      {posts.map(({ title, slug, eyecatch }) => (
+    <div className={styles.postsContainer}>
+      {posts.map(({ title, slug, eyecatch, publishDate = '', categories }) => (
         <article className={styles.post} key={slug}>
-          <Link href={`/articles/${slug}`}>
+          <Link className={styles.link} href={`/articles/${slug}`}>
             <figure>
               <Image
                 src={eyecatch.url}
@@ -19,7 +22,20 @@ export default function Posts({ posts }) {
                 sizes="(min-width: 1152px) 576px, 50vw"
               />
             </figure>
-            <h2>{title}</h2>
+            <h2 className={styles.postsTitle}>{title}</h2>
+            <div className={flexContent}>
+              <div className={styles.publishDate}>
+                <ConvertDate dateISO={publishDate} />
+              </div>
+              <ul className={styles.postsCategoriesUl}>
+                {categories.map(({ name, slug }) => (
+                  <li className={styles.postsCategoriesLi} key={slug}>
+                    <FontAwesomeIcon className={styles.icon} icon={faTag} />
+                    <div className={styles.name}>{name}</div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </Link>
         </article>
       ))}
