556c44fe4186584fb31d0bea64c14087cfec9ae8
kakuta0915 <kakuta19960915@gmail.com>
2023-06-10 23:02:06 +0900

    🎨 socialコンポーネントのデザイン変更

--- src/components/social/social.jsx
+++ src/components/social/social.jsx
@@ -8,16 +8,16 @@ export default function Social() {
   return (
     <ul className={styles.socialList}>
       <li>
-        <a href="https://twitter.com/home">
+        <Link href="https://twitter.com/home">
           <FontAwesomeIcon icon={faTwitter} />
           <span className="sr-only">Twitter</span>
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="https://github.com/kakuta0915">
+        <Link href="https://github.com/kakuta0915">
           <FontAwesomeIcon icon={faGithub} />
           <span className="sr-only">GitHub</span>
-        </a>
+        </Link>
       </li>
     </ul>
   )
