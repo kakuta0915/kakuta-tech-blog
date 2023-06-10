4cb5511422f6d6111a173ccd597e0f20f65733b8
kakuta0915 <kakuta19960915@gmail.com>
2023-06-10 22:38:03 +0900

    🎨 headerの細かいデザイン変更

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,6 @@
 import Logo from '../logo/logo'
 import Nav from '../nav/nav'
 import styles from './header.module.css'
-import Container from '../container/container'
 
 export default function Header() {
   return (
