f61ab2b539c42544998540da052440248f22c939
kakuta0915 <kakuta19960915@gmail.com>
2023-06-15 07:35:12 +0900

    💄 profileコンポーネントのレイアウト変更

--- pages/about.jsx
+++ pages/about.jsx
@@ -29,63 +29,8 @@ export default function About() {
             placeholder="blur"
           />
         </figure>
+        <Profile />
       </Container>
-
-      <Profile>
-        <h3>このサイトについて</h3>
-        <p>
-          このサイト主の「実績紹介」「WEB技術の記事」などをまとめたサイトです。
-        </p>
-
-        <h3>プロフィール</h3>
-        <p>独学でプログラミング学習をしています。</p>
-
-        <h3>趣味</h3>
-        <p>アニメ、筋トレ、絵描き、読書、散歩</p>
-
-        <h3>学習した技術・ツール</h3>
-
-        <table>
-          <tbody>
-            <tr>
-              <th>フロントエンド</th>
-              <td>html5 / CSS3 / JavaScript / Sass / TypeScript</td>
-            </tr>
-            <tr>
-              <th>サーバサイド</th>
-              <td>node.js</td>
-            </tr>
-            <tr>
-              <th>フレームワーク</th>
-              <td>jQuery / Next.js</td>
-            </tr>
-            <tr>
-              <th>ライブラリ</th>
-              <td>React / Veu.js</td>
-            </tr>
-            <tr>
-              <th>ツール</th>
-              <td>VSCode / Git / Gulp</td>
-            </tr>
-            <tr>
-              <th>パッケージ管理</th>
-              <td>npm / yarn / Homebrew</td>
-            </tr>
-            <tr>
-              <th>データベース</th>
-              <td>MySQL / PostgreSQL</td>
-            </tr>
-            <tr>
-              <th>プラットフォーム・OS関係</th>
-              <td>Linux / Mac / Firebase / GitHub</td>
-            </tr>
-            <tr>
-              <th>仮想化・コンテナ技術</th>
-              <td>Docker</td>
-            </tr>
-          </tbody>
-        </table>
-      </Profile>
     </>
   )
 }
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,5 +1,27 @@
 import styles from './profile.module.css'
 
-export default function Profile({ children }) {
-  return <div className={styles.profile}>{children}</div>
+export default function Profile() {
+  return (
+    <div className={styles.profile}>
+      <h3 className={styles.profileTitle}>このサイトについて</h3>
+      <p className={styles.profileText}>
+        このサイト主の「実績紹介」「WEB技術の記事」などをまとめたサイトです。
+      </p>
+
+      <h3 className={styles.profileTitle}>プロフィール</h3>
+      <p className={styles.profileText}>
+        独学でプログラミング学習をしています。
+      </p>
+
+      <h3 className={styles.profileTitle}>趣味</h3>
+      <p className={styles.profileText}>アニメ、筋トレ、絵描き、読書、散歩</p>
+
+      <h3 className={styles.profileTitle}>学習した技術・ツール</h3>
+      <p className={styles.profileText}>
+        html5 / CSS3 / JavaScript / Sass / TypeScript / node.js / jQuery /
+        Next.js / React / Veu.js / VSCode / Git / Gulp / npm / yarn / Homebrew /
+        microCMS / MySQL / Linux / Mac / Firebase / GitHub / Docker
+      </p>
+    </div>
+  )
 }
