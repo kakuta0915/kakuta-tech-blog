55233e7fddbd35dd12fe170c5e0ce75e36e0c99d
kakuta0915 <kakuta19960915@gmail.com>
2023-06-18 19:38:12 +0900

    ♻️ お問い合わせページのリファクタリング

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -2,6 +2,41 @@
 
 import styles from './form.module.css'
 
-export default function Form({ children }) {
-  return <div className={styles.stack}>{children}</div>
+export default function Form() {
+  return (
+    <div className={styles.stack}>
+      <form>
+        <ul>
+          <li>
+            <label for="username">
+              お名前
+              <span>【必須】</span>
+            </label>
+            <input type="text" id="username" name="username" />
+          </li>
+          <li>
+            <label for="company">会社名</label>
+            <input type="text" id="company" name="company" />
+          </li>
+          <li>
+            <label for="email">
+              メールアドレス
+              <span>【必須】</span>
+            </label>
+            <input type="email" id="email" name="email" />
+          </li>
+          <li>
+            <label for="detail">
+              お問い合わせ内容
+              <span>【必須】</span>
+            </label>
+            <textarea id="detail" name="detail"></textarea>
+          </li>
+        </ul>
+      </form>
+      <button type="submit" value="送信">
+        送信する
+      </button>
+    </div>
+  )
 }
