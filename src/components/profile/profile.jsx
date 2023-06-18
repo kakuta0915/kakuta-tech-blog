5ccd41461fcab01c2a27ae878398d66ab140f2a4
kakuta0915 <kakuta19960915@gmail.com>
2023-06-19 08:10:06 +0900

    💄 profileコンポーネントのレイアウト変更

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -3,21 +3,19 @@ import styles from './profile.module.css'
 export default function Profile() {
   return (
     <div className={styles.profile}>
-      <h3 className={styles.profileTitle}>このサイトについて</h3>
-      <p className={styles.profileText}>
+      <h3>このサイトについて</h3>
+      <p>
         このサイト主の「実績紹介」「WEB技術の記事」などをまとめたサイトです。
       </p>
 
-      <h3 className={styles.profileTitle}>プロフィール</h3>
-      <p className={styles.profileText}>
-        独学でプログラミング学習をしています。
-      </p>
+      <h3>プロフィール</h3>
+      <p>独学でプログラミング学習をしています。</p>
 
-      <h3 className={styles.profileTitle}>趣味</h3>
-      <p className={styles.profileText}>アニメ、筋トレ、絵描き、読書、散歩</p>
+      <h3>趣味</h3>
+      <p>アニメ、筋トレ、絵描き、読書、散歩</p>
 
-      <h3 className={styles.profileTitle}>学習した技術・ツール</h3>
-      <p className={styles.profileText}>
+      <h3>学習した技術・ツール</h3>
+      <p>
         html5 / CSS3 / JavaScript / Sass / TypeScript / node.js / jQuery /
         Next.js / React / Veu.js / VSCode / Git / Gulp / npm / yarn / Homebrew /
         microCMS / MySQL / Linux / Mac / Firebase / GitHub / Docker
