2a841ed5a26da14caeb4817f78b195c6e5dd2052
kakuta0915 <kakuta19960915@gmail.com>
2023-07-11 19:13:03 +0900

    ✨ 💄 TOPページにTopAboutコンポーネントを追加した

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,37 @@
+import { getAllPosts } from '@/libs/api'
 import Meta from '@/src/components/meta/meta'
 import Container from '@/src/components/container/container'
 import Hero from '@/src/components/hero/hero'
 import Image from 'next/image'
 import eyecatch from 'images/index.jpg'
-import { getAllPosts } from '@/libs/api'
+import TopAbout from '@/src/components/top-about/top-about'
 import Posts from '@/src/components/posts/posts'
 
 export default function Home({ posts }) {
   return (
-    <>
-      <Container>
-        <Meta
-          pageTitle="TOP"
-          pageDesc="プログラミング学習記録をまとめたサイト"
-          pageImg={eyecatch.src}
-          pageImgW={eyecatch.width}
-          pageImgH={eyecatch.height}
+    <Container>
+      <Meta
+        pageTitle="TOP"
+        pageDesc="プログラミング学習記録をまとめたサイト"
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <Hero title="TOP" subtitle="トップページ" />
+      <figure style={{ padding: '1rem' }}>
+        <Image
+          src={eyecatch}
+          alt=""
+          layout="responsive"
+          sizes="(min-width: 1152px) 1152px, 100vw"
+          priority
+          placeholder="blur"
         />
-        <Hero title="TOP" subtitle="トップページ" />
-        <figure style={{ padding: '1rem' }}>
-          <Image
-            src={eyecatch}
-            alt=""
-            layout="responsive"
-            sizes="(min-width: 1152px) 1152px, 100vw"
-            priority
-            placeholder="blur"
-          />
-        </figure>
+      </figure>
+      <TopAbout />
 
-        <Posts posts={posts} />
-      </Container>
-    </>
+      <Posts posts={posts} btn />
+    </Container>
   )
 }
 
