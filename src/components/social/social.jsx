e4027d5d0f51e5f9af897328f88d6dd637e7829c
kakuta0915 <kakuta19960915@gmail.com>
2023-07-11 20:44:38 +0900

    ✨ socialのリンクの修正

--- src/components/social/social.jsx
+++ src/components/social/social.jsx
@@ -8,13 +8,21 @@ export default function Social() {
   return (
     <ul className={styles.socialList}>
       <li>
-        <Link href="https://twitter.com/_takeru0915_">
+        <Link
+          href="https://twitter.com/_takeru0915_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faTwitter} />
           <span className="sr-only">Twitter</span>
         </Link>
       </li>
       <li>
-        <Link href="https://github.com/kakuta0915">
+        <Link
+          href="https://github.com/kakuta0915"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faGithub} />
           <span className="sr-only">GitHub</span>
         </Link>
