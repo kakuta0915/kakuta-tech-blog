18339f65a8a045b47e0805c453b68c63afb9ce49
kakuta0915 <kakuta19960915@gmail.com>
2023-07-17 15:52:02 +0900

    ✨ 💄 post-categoresにアイコンを追加した

--- src/components/post-categories/post-categories.jsx
+++ src/components/post-categories/post-categories.jsx
@@ -1,22 +1,27 @@
 // 記事に関連する特定のタグをリスト表示する機能
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './post-categories.module.css'
 import Link from 'next/link'
-import { faTag } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
 
 export default function PostCategories({ categories }) {
   return (
     <div className={styles.flexContainer}>
       <h3>カテゴリ</h3>
       <ul>
-        {categories.map(({ name, slug }) => (
+        {categories.map(({ name, slug, icon }) => (
           <li key={slug}>
             <Link
               className={styles.categoriesLink}
               href={`/articles/categories/${slug}`}
             >
-              <FontAwesomeIcon className={styles.tagsIcon} icon={faTag} />
+              <Image
+                className={styles.icon}
+                src={icon.url}
+                width={icon.width}
+                height={icon.height}
+                alt="画像"
+              />
               {name}
             </Link>
           </li>
