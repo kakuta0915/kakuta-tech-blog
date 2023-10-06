f655aa9723d5e8b6363b8f1be48cc98766a486fb
kakuta0915 <kakuta19960915@gmail.com>
2023-10-06 19:40:40 +0900

    :zap: 目次をh2のみ取得するように変更した。

--- libs/render-toc.js
+++ libs/render-toc.js
@@ -4,7 +4,7 @@ import * as cheerio from 'cheerio'
 
 export const renderToc = (body) => {
   const $ = cheerio.load(body)
-  const headings = $('h1, h2').toArray()
+  const headings = $('h2').toArray()
   const toc = headings.map((data) => ({
     text: data.children[0].data,
     id: data.attribs.id,
