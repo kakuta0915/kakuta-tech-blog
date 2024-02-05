ed8b457fd8754f8ac666be9e478aecd817344c7e
kakuta0915 <kakuta19960915@gmail.com>
2024-02-05 19:42:27 +0900

    修正

--- pages/about/page.jsx
+++ pages/about/page.jsx
@@ -1,7 +1,7 @@
 // aboutページ
 
 import Meta from '@/src/components/elements/Meta/Meta'
-import Container from '@/src/components/layouts/Container'
+import Container from '@/src/components/layouts/Container/Container'
 import Hero from '@/src/components/elements/Hero/Hero'
 import Image from 'next/image'
 import eyecatch from 'images/about.jpg'
