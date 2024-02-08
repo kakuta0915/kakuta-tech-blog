7e3480ab649ec8d6ead22c0bd149df2067482a1d
Takeru Kakuta <kakuta19960915@gmail.com>
2024-02-08 20:30:38 +0900

    変更

--- src/components/layouts/container/container.jsx
+++ src/components/layouts/container/container.jsx
@@ -1,5 +1,5 @@
 // 横幅の共通レイアウト
-import styles from './container.module.css'
+import styles from './Container.module.css'
 
 export default function Container({ children, large = false }) {
   return <div className={large ? styles.large : styles.default}>{children}</div>
