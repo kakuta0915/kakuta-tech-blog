5327df5d45d70a6ac6bd2be90d90c01d030765e5
Takeru Kakuta <kakuta19960915@gmail.com>
2024-02-09 16:02:55 +0900

    変更

---
+++ pages/_document.jsx
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { siteMeta } from '@/libs/constants'
+const { siteLang } = siteMeta
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang={siteLang}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
