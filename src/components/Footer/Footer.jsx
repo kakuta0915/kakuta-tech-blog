97abef1e298e318236f29534adcfbc2d55230edf
Takeru Kakuta <kakuta19960915@gmail.com>
2024-06-28 17:25:48 +0900

    Logoコンポーネントのレイアウト・コード修正

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-
 import styles from './Footer.module.css'
 import Social from '../Social/Social'
 import Logo from '../Logo/Logo'
@@ -7,7 +6,7 @@ export default function Footer() {
   return (
     <footer className={styles.footerContainer}>
       <div className={styles.footerText}>
-        <Logo footerLink />
+        <Logo isFooterLogo={true} />
         <Social />
       </div>
     </footer>
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,14 +3,18 @@
 import Link from 'next/link'
 import styles from './Logo.module.css'
 import Image from 'next/image'
-import logo from '@/images/logo.png'
+import headerLogo from '@/images/headerLogo.png'
+import footerLogoImage from '@/images/footerLogo.png'
 
-export default function Logo({ footerLink = false }) {
+export default function Logo({ isFooterLogo = false }) {
   return (
-    <Link href="/" className={footerLink ? styles.footerLink : styles.headerLink}>
+    <Link
+      href="/"
+      className={isFooterLogo ? styles.footerLogo : styles.headerLogo}
+    >
       <Image
         className={styles.linkImage}
-        src={logo}
+        src={isFooterLogo ? footerLogoImage : headerLogo}
         alt="Logo Image"
         priority
       />
