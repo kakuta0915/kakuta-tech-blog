ca626c0195af1dc84679296ab1992d32d3c8eb69
Takeru Kakuta <kakuta19960915@gmail.com>
2024-06-28 20:24:09 +0900

    💄 styles: Heroコンポーネントのレイアウト修正

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,67 +1,62 @@
 // Contactページ
+import Image from 'next/image'
 import Meta from '@/src/components/Meta/Meta'
 import Hero from '@/src/components/Hero/Hero'
-import Image from 'next/image'
-import eyecatch from 'images/contact.jpg'
-import styles from '@/src/styles/contact.module.css'
 import Container from '@/src/components/Container/Container'
+import styles from '@/src/styles/contact.module.css'
+import eyecatch from 'images/contact.jpg'
 
 export default function Contact() {
   return (
-    <Container>
+    <>
       <Meta
         pageTitle="CONTACT"
-        pageDesc="お問い合わせはこちらからお願いいたします"
+        pageDesc="ご質問やご意見、お仕事の依頼などをお待ちしています。お気軽に以下のフォームからご連絡ください。"
         pageImg={eyecatch.src}
         pageImgW={eyecatch.width}
         pageImgH={eyecatch.height}
       />
-      <div className={styles.contact}>
-        <Hero title="CONTACT" subtitle="お問い合わせ" />
-        <figure>
-          <Image
-            src={eyecatch}
-            alt=""
-            layout="responsive"
-            sizes="(min-width: 1152px) 1152px, 100vw"
-            priority
-            placeholder="blur"
-          />
-        </figure>
-
-        <form className={styles.form}>
-          <ul>
-            <li>
-              <label for="username">
-                お名前
-                <span>【必須】</span>
-              </label>
-              <input type="text" id="username" name="username" />
-            </li>
-            <li>
-              <label for="company">会社名</label>
-              <input type="text" id="company" name="company" />
-            </li>
-            <li>
-              <label for="email">
-                メールアドレス
-                <span>【必須】</span>
-              </label>
-              <input type="email" id="email" name="email" />
-            </li>
-            <li>
-              <label for="detail">
-                お問い合わせ内容
-                <span>【必須】</span>
-              </label>
-              <textarea id="detail" name="detail"></textarea>
-            </li>
-          </ul>
-        </form>
-        <button className={styles.button} type="submit" value="送信">
-          送信する
-        </button>
-      </div>
-    </Container>
+      <Hero
+        title="CONTACT"
+        description="ご質問やご意見、お仕事の依頼などをお待ちしています。お気軽に以下のフォームからご連絡ください。"
+        imageSrc="./images/contact.jpg"
+      />
+      <Container>
+        <div className={styles.contact}>
+          <form className={styles.form}>
+            <ul>
+              <li>
+                <label for="username">
+                  お名前
+                  <span>【必須】</span>
+                </label>
+                <input type="text" id="username" name="username" />
+              </li>
+              <li>
+                <label for="company">会社名</label>
+                <input type="text" id="company" name="company" />
+              </li>
+              <li>
+                <label for="email">
+                  メールアドレス
+                  <span>【必須】</span>
+                </label>
+                <input type="email" id="email" name="email" />
+              </li>
+              <li>
+                <label for="detail">
+                  お問い合わせ内容
+                  <span>【必須】</span>
+                </label>
+                <textarea id="detail" name="detail"></textarea>
+              </li>
+            </ul>
+          </form>
+          <button className={styles.button} type="submit" value="送信">
+            送信する
+          </button>
+        </div>
+      </Container>
+    </>
   )
 }
--- pages/works.jsx
+++ pages/works.jsx
@@ -1,11 +1,11 @@
 // worksページ
+import Image from 'next/image'
 import Meta from '@/src/components/Meta/Meta'
 import Hero from '@/src/components/Hero/Hero'
-import eyecatch from 'images/works.jpg'
-import Image from 'next/image'
-import styles from '@/src/styles/works.module.css'
 import Container from '@/src/components/Container/Container'
 import Worklist from '@/src/components/Worklist/Worklist'
+import styles from '@/src/styles/works.module.css'
+import eyecatch from 'images/works.jpg'
 
 const worksData = [
   {
@@ -21,25 +21,20 @@ const worksData = [
 export default function Works() {
   return (
     <>
+      <Meta
+        pageTitle="WORKS"
+        description="独学で制作したサイトなどを掲載しており、GitHubからコードもご覧いただけます。"
+        pageDesc=""
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <Hero
+        title="WORKS"
+        description="独学で制作したサイトなどを掲載しており、GitHubからコードもご覧いただけます。"
+        imageSrc="./images/works.jpg"
+      />
       <Container>
-        <Meta
-          pageTitle="WORKS"
-          pageDesc="制作物一覧のページです"
-          pageImg={eyecatch.src}
-          pageImgW={eyecatch.width}
-          pageImgH={eyecatch.height}
-        />
-        <Hero title="WORKS" subtitle="自主制作一覧" />
-        <figure style={{ padding: '1rem' }}>
-          <Image
-            src={eyecatch}
-            alt=""
-            layout="responsive"
-            sizes="(min-width: 1152px) 1152px, 100vw"
-            priority
-            placeholder="blur"
-          />
-        </figure>
         <section className={styles.worksList}>
           <Worklist worksData={worksData} />
         </section>
