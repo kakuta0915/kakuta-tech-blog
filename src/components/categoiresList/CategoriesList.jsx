24a402d89dace731dc6d3aab28fba92351f07ff1
Takeru Kakuta <kakuta19960915@gmail.com>
2024-07-03 19:39:36 +0900

    ディレクトリ名の変更

---
+++ src/components/categoiresList/CategoriesList.jsx
@@ -0,0 +1,57 @@
+// すべてのカテゴリーをカテゴリー / スラッグページに追加
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState, useRef } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleDown } from '@fortawesome/free-solid-svg-icons'
+import styles from './categoriesList.module.css'
+
+export default function CategoriesList({ allCategories }) {
+  const [categoriesOpen, setCategoriesOpen] = useState(false)
+  const toggleCategories = () => {
+    setCategoriesOpen((prev) => !prev)
+  }
+  const refCategories = useRef(null)
+
+  return (
+    <div
+      className={`${styles.categories} ${
+        categoriesOpen ? styles.open : styles.close
+      }`}
+    >
+      <div className={styles.categoriesBtn} onClick={toggleCategories}>
+        <h3>カテゴリ 一覧</h3>
+        <FontAwesomeIcon className={styles.icon} icon={faCircleDown} />
+      </div>
+      <div
+        className={styles.accordion}
+        ref={refCategories}
+        style={{
+          '--categories-height': refCategories.current
+            ? `${refCategories.current.scrollHeight}px`
+            : '0px',
+        }}
+      >
+        <ul>
+          {allCategories.map(({ name, slug, icon }) => (
+            <li className={styles.categoriesList} key={slug}>
+              <Link
+                className={styles.categoriesLink}
+                href={`/articles/categories/${slug}`}
+              >
+                <Image
+                  className={styles.iconImg}
+                  src={icon.url}
+                  width={icon.width}
+                  height={icon.height}
+                  alt=""
+                />
+                <span className={styles.name}>{name}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
