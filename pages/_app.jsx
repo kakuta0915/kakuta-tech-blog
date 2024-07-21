41c50aaaf762f4e04c1a2b2374fb8373bf5c6217
Takeru Kakuta <kakuta19960915@gmail.com>
2024-07-21 10:39:22 +0900

    変更

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router'
 import Script from 'next/script'
 import * as gtag from 'libs/gtag'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import Layout from '@/src/components/layouts/Layout'
+import Layout from '@/src/components/layouts/layout'
 import styles from '@/src/styles/globals.css'
 
 // Font Awesomeの設定
