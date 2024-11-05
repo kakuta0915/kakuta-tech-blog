a7007224571df41a09c80f462eb68572ee3e2c21
Takeru Kakuta <kakuta19960915@gmail.com>
2024-11-05 19:33:54 +0900

    ✨ feat: ログイン画面に「メールアドレスでログイン」する機能とUIの実装

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: 'AIzaSyAMClWGguKQ6jD63ZIHr2EZJCF5CzhgzLs',
   authDomain: 'kakuta-tech-blog.firebaseapp.com',
   projectId: 'kakuta-tech-blog',
-  storageBucket: 'kakuta-tech-blog.appspot.com',
+  storageBucket: 'kakuta-tech-blog.firebasestorage.app',
   messagingSenderId: '167476717749',
   appId: '1:167476717749:web:5e001798584c908c06f6fc',
   measurementId: 'G-MJC3MXXLBG',
--- src/components/authService/authService.jsx
+++ src/components/authService/authService.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef, useCallback } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { auth, provider } from '@/firebaseConfig'
-import { signInWithPopup } from 'firebase/auth'
+import { signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import styles from './authService.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,6 +11,7 @@ import {
   faGear,
   faHeart,
   faRightToBracket,
+  faTimes,
   faUser,
 } from '@fortawesome/free-solid-svg-icons'
 import googleIcon from '@/public/images/googleIcon.png'
@@ -26,6 +27,9 @@ function AuthService() {
 
 function SignInWithGoogle() {
   const [showPopup, setShowPopup] = useState(false)
+  const [showOtherOptions, setShowOtherOptions] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleSignIn = () => {
     signInWithPopup(auth, provider).catch((error) => {
@@ -33,13 +37,38 @@ function SignInWithGoogle() {
     })
   }
 
+  const handleEmailSignIn = async () => {
+    if (email.endsWith('@gmail.com')) {
+      alert(
+        'Googleのメールアドレスは使用できません。別のメールアドレスを入力してください。',
+      )
+      return
+    }
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+      alert('ログイン成功！！！！！！！！')
+    } catch (error) {
+      console.error('Error signing in with email: ', error)
+    }
+  }
+
   const togglePopup = () => {
-    setShowPopup((prev) => !prev)
+    setShowPopup((prev) => {
+      if (prev) {
+        setShowOtherOptions(false)
+      }
+      return !prev
+    })
+  }
+
+  const handleShowOtherOptions = () => {
+    setShowOtherOptions(true)
   }
 
   return (
     <>
-      <button onClick={togglePopup} className={styles.button}>
+      <button onClick={togglePopup} className={styles.loginButton}>
         <FontAwesomeIcon icon={faRightToBracket} className={styles.icon} />
         Log in
       </button>
@@ -49,20 +78,52 @@ function SignInWithGoogle() {
             className={styles.popupContent}
             onClick={(e) => e.stopPropagation()}
           >
+            <button className={styles.closeButton} onClick={togglePopup}>
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
             <h3>KAKUTA TECH BLOG</h3>
-            <p>ログインして投稿を管理しましょう</p>
+            <p>ログインして記事を保存したり、コメントをしましょう。</p>
             <button onClick={handleSignIn} className={styles.googleButton}>
               <Image
                 src={googleIcon}
-                alt="googleでログイン"
+                alt="Googleでログイン"
                 width={18}
                 height={18}
               />
               <span>Googleでログイン</span>
             </button>
-            <p className={styles.noAccountText}>
-              Googleアカウントをお持ちでない方
-            </p>
+
+            {!showOtherOptions && (
+              <button
+                className={styles.noAccountText}
+                onClick={handleShowOtherOptions}
+              >
+                Googleアカウントをお持ちでない方
+              </button>
+            )}
+
+            {showOtherOptions && (
+              <div className={styles.otherOptionsPopup}>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="メールアドレス"
+                />
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="パスワード"
+                />
+                <button
+                  className={styles.optionButton}
+                  onClick={handleEmailSignIn}
+                >
+                  メールでログイン
+                </button>
+              </div>
+            )}
           </div>
         </div>
       )}
@@ -122,7 +183,7 @@ function UserInfo({ user }) {
             <FontAwesomeIcon icon={faGear} className={styles.icon} />
             アカウント設定
           </Link>
-          <button onClick={() => auth.signOut()} className={styles.button}>
+          <button onClick={() => auth.signOut()} className={styles.loginButton}>
             ログアウト
           </button>
         </div>
