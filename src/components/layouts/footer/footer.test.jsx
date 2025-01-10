d6bb7f4ba1175b89967b87c76d0d91f5e1a87e6a
Takeru Kakuta <kakuta19960915@gmail.com>
2025-01-10 21:19:39 +0900

    ✅ test: Footerコンポーネントのテスト

--- src/components/layouts/footer/footer.test.jsx
+++ src/components/layouts/footer/footer.test.jsx
@@ -3,16 +3,16 @@ import Footer from './footer'
 import '@testing-library/jest-dom'
 
 describe('Footer component', () => {
-  // Logoコンポーネントがレンダリングされるかをテスト
-  it('renders the Logo component', () => {
+  it('Logoコンポーネントがレンダリングされるか', () => {
     render(<Footer />)
+
     const logoElement = screen.getByRole('img', { name: /logo/i })
     expect(logoElement).toBeInTheDocument()
   })
 
-  // Socialコンポーネントがレンダリングされるかをテスト
-  it('renders the Social component', () => {
+  it('Socialコンポーネントがレンダリングされるか', () => {
     render(<Footer />)
+
     const socialElement = screen.getByTestId('social')
     expect(socialElement).toBeInTheDocument()
   })
