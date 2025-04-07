db474040c94ed269ed173689e9eceac9c03233b7
Takeru Kakuta <kakuta19960915@gmail.com>
2025-04-07 17:35:55 +0900

    ⚡️ types: TableOfContentsコンポーネントに型を定義した

--- src/components/TableOfContents/index.test.tsx
+++ src/components/TableOfContents/index.test.tsx
@@ -9,15 +9,12 @@ const mockToc = [
 test('目次が正しく表示されるか確認', () => {
   render(<TableOfContents toc={mockToc} />)
 
-  // 目次のタイトルが表示されているか確認
   expect(screen.getByText('目次')).toBeInTheDocument()
 
-  // 各項目が表示されているか確認
   mockToc.forEach(({ id, text }) => {
     const listItem = screen.getByText(text)
     expect(listItem).toBeInTheDocument()
 
-    // data-testid属性を使ってスクロールリンクを確認する
     const link = screen.getByTestId(`scroll-link-${id}`)
     expect(link).toBeInTheDocument()
     expect(link).toHaveTextContent(text)
--- src/components/TableOfContents/index.tsx
+++ src/components/TableOfContents/index.tsx
@@ -1,36 +1,54 @@
-// 目次コンポーネント
 import { useRef, useState } from 'react'
 import { Link as Scroll } from 'react-scroll'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleDown } from '@fortawesome/free-solid-svg-icons'
 import styles from './index.module.css'
 
-export default function TableOfContents({ toc }) {
+type TocItem = {
+  id: string
+  text: string
+}
+
+interface TableOfContentsProps {
+  toc: TocItem[]
+}
+
+interface CustomStyle extends React.CSSProperties {
+  '--toc-height'?: string
+}
+
+const TableOfContents: React.FC<TableOfContentsProps> = ({ toc }) => {
   const [tocOpen, setTocOpen] = useState(false)
   const toggleToc = () => {
     setTocOpen((prev) => !prev)
   }
-  const refToc = useRef(null)
+  const refToc = useRef<HTMLDivElement>(null)
 
   return (
-    <div className={`${styles.toc} ${tocOpen ? styles.open : styles.close}`}>
-      <div className={styles.tocBtn} onClick={toggleToc}>
+    <div
+      className={`${styles['toc']} ${
+        tocOpen ? styles['open'] : styles['close']
+      }`}
+    >
+      <div className={styles['tocBtn']} onClick={toggleToc}>
         <h3>目次</h3>
-        <FontAwesomeIcon className={styles.icon} icon={faCircleDown} />
+        <FontAwesomeIcon className={styles['icon']} icon={faCircleDown} />
       </div>
       <div
-        className={styles.accordion}
+        className={styles['accordion']}
         ref={refToc}
-        style={{
-          '--toc-height': refToc.current
-            ? `${refToc.current.scrollHeight}px`
-            : '0px',
-        }}
+        style={
+          {
+            '--toc-height': refToc.current
+              ? `${refToc.current.scrollHeight}px`
+              : '0px',
+          } as CustomStyle
+        }
       >
         <ul>
           {toc.map((data) => (
             <Scroll
-              className={styles.scroll}
+              className={styles['scroll']}
               key={data.id}
               activeClass="active"
               to={data.id}
@@ -40,7 +58,7 @@ export default function TableOfContents({ toc }) {
               duration={800}
               data-testid={`scroll-link-${data.id}`}
             >
-              <li className={styles.tocLi} key={data.id}>
+              <li className={styles['tocLi']} key={data.id}>
                 {data.text}
               </li>
             </Scroll>
@@ -50,3 +68,5 @@ export default function TableOfContents({ toc }) {
     </div>
   )
 }
+
+export default TableOfContents
