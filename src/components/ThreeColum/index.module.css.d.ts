44c2e5914b192dcad8994fa9571bfa1c0bfb2016
Takeru Kakuta <kakuta19960915@gmail.com>
2025-04-09 12:12:57 +0900

    ThreeColumを修正

--- src/components/ThreeColum/index.module.css.d.ts
+++ src/components/ThreeColum/index.module.css.d.ts
@@ -2,3 +2,12 @@ declare module '*.module.css' {
   const content: { [className: string]: string }
   export default content
 }
+
+declare const styles: {
+  flexContainer: string
+  main: string
+  sidebar: string
+  socialActions: string
+}
+
+export default styles
