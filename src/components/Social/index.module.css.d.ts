a8fe0671182655e26a9d24c409290104f89b4e11
Takeru Kakuta <kakuta19960915@gmail.com>
2025-04-09 12:18:06 +0900

    Socialの型を修正

--- src/components/Social/index.module.css.d.ts
+++ src/components/Social/index.module.css.d.ts
@@ -2,3 +2,12 @@ declare module '*.module.css' {
   const content: { [className: string]: string }
   export default content
 }
+
+declare const styles: {
+  socialListFooter: string
+  socialList: string
+  link: string
+  icon: string
+}
+
+export default styles
