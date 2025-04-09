6d4b5ebe14edf2f2984991fcc20f7985a49b0e48
Takeru Kakuta <kakuta19960915@gmail.com>
2025-04-09 12:22:10 +0900

    ⚡️ types: Postsの型を修正

--- src/components/Posts/index.module.css.d.ts
+++ src/components/Posts/index.module.css.d.ts
@@ -2,3 +2,19 @@ declare module '*.module.css' {
   const content: { [className: string]: string }
   export default content
 }
+
+declare const styles: {
+  postsContainer: string
+  link: string
+  publishDate: string
+  flexSocialActions: string
+  socialActions: string
+  icon: string
+  postsCategoriesLi: string
+  tagIcon: string
+  name: string
+  btnBox: string
+  btn: string
+}
+
+export default styles
