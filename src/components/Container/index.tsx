1b255a40e8885a087eb69b6f463707c1ee6b6be7
Takeru Kakuta <kakuta19960915@gmail.com>
2025-04-10 13:03:39 +0900

    ⚡️ types: Containerの型を定義

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import styles from './index.module.css'
+
+interface ContainerProps {
+  children: ReactNode
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div className={styles['container']}>{children}</div>
+}
+
+export default Container
