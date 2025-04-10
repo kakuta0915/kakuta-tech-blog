c78ba35e5669bff299dd31515140acae97d5fb9a
Takeru Kakuta <kakuta19960915@gmail.com>
2025-04-10 12:33:16 +0900

    ⚡️ types: Convertの型を定義

--- src/components/Convert/ConvertBody.test.tsx
+++ src/components/Convert/ConvertBody.test.tsx
@@ -1,36 +1,43 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable react/display-name */
-import React from 'react'
 import { render } from '@testing-library/react'
-import ConvertBody from '.'
+import ConvertBody from './ConvertBody'
 
-// モック設定
 jest.mock('next/image', () => ({
   __esModule: true,
-  default: ({ src, width, height, alt }) => (
-    <img src={src} width={width} height={height} alt={alt} />
-  ),
+  default: ({
+    src,
+    width,
+    height,
+    alt,
+  }: {
+    src: string
+    width: number
+    height: number
+    alt: string
+  }) => <img src={src} width={width} height={height} alt={alt} />,
 }))
 
-jest.mock('html-react-parser', () => (html) => {
-  if (html.includes('<img')) {
-    return <img src="/image.jpg" alt="Example image" width="600" height="400" />
+jest.mock('html-react-parser', () => {
+  return (html: string) => {
+    if (html.includes('<img')) {
+      return (
+        <img src="/image.jpg" alt="Example image" width={600} height={400} />
+      )
+    }
+    if (html.includes('<code')) {
+      return (
+        <code className="hljs">
+          <span className="hljs-keyword">const</span>
+        </code>
+      )
+    }
+    if (html.includes('<p')) {
+      return <p>This is a paragraph.</p>
+    }
+    return html
   }
-  if (html.includes('<code')) {
-    return (
-      <code className="hljs">
-        <span className="hljs-keyword">const</span>
-      </code>
-    )
-  }
-  if (html.includes('<p')) {
-    return <p>This is a paragraph.</p>
-  }
-  return html
 })
 
 describe('ConvertBody Component', () => {
-  // contentHTMLに渡されたimgタグが正しくレンダリングされるかをテスト
   it('renders an image tag correctly', () => {
     const contentHTML =
       '<img src="/image.jpg" alt="Example image" width="600" height="400" />'
@@ -42,17 +49,17 @@ describe('ConvertBody Component', () => {
     expect(img).toHaveAttribute('height', '400')
   })
 
-  // contentHTMLに渡されたcodeタグが正しくレンダリングされるかをテスト
   it('renders a code block correctly with syntax highlighting', () => {
     const contentHTML = '<code>const a = 1;</code>'
     const { container } = render(<ConvertBody contentHTML={contentHTML} />)
     const codeBlock = container.querySelector('code')
     expect(codeBlock).toBeInTheDocument()
-    expect(codeBlock).toHaveClass('hljs')
-    expect(codeBlock.innerHTML).toContain('hljs-keyword')
+    if (codeBlock) {
+      expect(codeBlock).toHaveClass('hljs')
+      expect(codeBlock.innerHTML).toContain('hljs-keyword')
+    }
   })
 
-  // contentHTMLに渡されたpタグが正しくレンダリングされるかをテスト
   it('renders other HTML elements correctly', () => {
     const contentHTML = '<p>This is a paragraph.</p>'
     const { container } = render(<ConvertBody contentHTML={contentHTML} />)
