65162ebf3c2347f55defefcd229666011dac444c
Takeru Kakuta <kakuta19960915@gmail.com>
2025-06-03 09:37:21 +0900

    ⚡️ types: Topページの型を指定

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,18 +8,25 @@ import Posts from '@/src/components/Posts'
 import Social from '@/src/components/Social'
 import PortfolioList from '@/src/components/PortfolioList'
 import styles from '@/src/styles/index.module.css'
-import useScrollAnimation from '@/src/components/UseScrollAnimation'
-import useScrollAnimationStyles from '@/src/components/UseScrollAnimation/index.module.css'
 import eyecatch from '@/public/images/index.jpg'
 import kakuta0915 from '@/public/images/kakuta0915.png'
 
-export default function Home({ posts }) {
-  useScrollAnimation([
-    `.${useScrollAnimationStyles.fadeInUp}`,
-    `.${useScrollAnimationStyles.fadeInRight}`,
-    `.${useScrollAnimationStyles.fadeInLeft}`,
-  ])
+type Post = {
+  title: string
+  slug: string
+  eyecatch: { url: string }
+  publishDate: string
+  categories: { name: string; slug: string }[]
+  source: string
+  likesCount: number
+  bookmarksCount: number
+}
 
+type HomeProps = {
+  posts: Post[]
+}
+
+const Home: React.FC<HomeProps> = ({ posts }) => {
   const portfolioData = [
     {
       id: 1,
@@ -48,54 +55,42 @@ export default function Home({ posts }) {
         contact={true}
       />
       <Container>
-        <section className={styles.aboutSection}>
-          <h2 className={useScrollAnimationStyles.fadeInUp}>About Me</h2>
-          <p className={`${styles.text} ${useScrollAnimationStyles.fadeInUp}`}>
+        <section className={styles['aboutSection']}>
+          <h2>About Me</h2>
+          <p className={styles['text']}>
             技術力の向上と実践的なスキル構築を目指し、自主的にプログラミングを学習しています。
             これまでの学習過程やスキルセットなど、私のプロフィールについてご紹介いたします。
           </p>
           <Image
-            className={`${styles.profileIcon} ${useScrollAnimationStyles.fadeInUp}`}
+            className={styles['profileIcon']}
             src={kakuta0915}
             alt=""
             objectFit="contain"
             priority
             placeholder="blur"
           />
-          <Social className={useScrollAnimationStyles.fadeInUp} />
-          <div
-            className={`${styles.btnBox} ${useScrollAnimationStyles.fadeInUp}`}
-          >
-            <Link className={styles.btn} href="./about/">
+          <Social isFooterSocial={false} />
+          <div className={styles['btnBox']}>
+            <Link className={styles['btn']} href="./about/">
               MORE
             </Link>
           </div>
         </section>
-        <section className={styles.articlesSection}>
-          <h2 className={useScrollAnimationStyles.fadeInUp}>Articles</h2>
-          <p className={`${styles.text} ${useScrollAnimationStyles.fadeInUp}`}>
+        <section className={styles['articlesSection']}>
+          <h2>Articles</h2>
+          <p className={styles['text']}>
             プログラミング学習に関する記事をまとめたページです。学習中に躓いた箇所や、開発過程で遭遇した具体的な課題、それに対する解決策を紹介しています。
           </p>
-          <Posts
-            className={useScrollAnimationStyles.fadeInUp}
-            posts={posts}
-            maxPosts={6}
-            btn
-          />
+          <Posts posts={posts} maxPosts={6} btn />
         </section>
-        <section className={styles.portfolioSection}>
-          <h2 className={useScrollAnimationStyles.fadeInUp}>Portfolio</h2>
-          <p className={`${styles.text} ${useScrollAnimationStyles.fadeInUp}`}>
+        <section className={styles['portfolioSection']}>
+          <h2>Portfolio</h2>
+          <p className={styles['text']}>
             独学で制作したサイトなどを掲載しており、GitHubからコードもご覧いただけます。
           </p>
-          <PortfolioList
-            portfolioData={portfolioData}
-            className={`${useScrollAnimationStyles.fadeInUp} `}
-          />
-          <div
-            className={`${useScrollAnimationStyles.fadeInUp} ${styles.btnBox}`}
-          >
-            <Link className={styles.btn} href="./portfolio/">
+          <PortfolioList portfolioData={portfolioData} className={''} />
+          <div className={styles['btnBox']}>
+            <Link className={styles['btn']} href="./portfolio/">
               MORE
             </Link>
           </div>
@@ -114,3 +109,5 @@ export async function getServerSideProps() {
     },
   }
 }
+
+export default Home
