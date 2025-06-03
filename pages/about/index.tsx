931c7ee9efa16c68e5d1b5c1512e554708df651e
Takeru Kakuta <kakuta19960915@gmail.com>
2025-06-03 10:30:16 +0900

    types: Aboutページの型定義

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,19 +3,11 @@ import Meta from '@/src/components/Meta'
 import Hero from '@/src/components/Hero'
 import Container from '@/src/components/Container'
 import Social from '@/src/components/Social'
-import useScrollAnimation from '@/src/components/UseScrollAnimation'
-import useScrollAnimationStyles from '@/src/components/UseScrollAnimation/index.module.css'
 import styles from './index.module.css'
 import eyecatch from '@/public/images/about.jpg'
 import kakuta0915 from '@/public/images/kakuta0915.png'
 
-export default function About() {
-  useScrollAnimation([
-    `.${useScrollAnimationStyles.fadeInUp}`,
-    `.${useScrollAnimationStyles.fadeInRight}`,
-    `.${useScrollAnimationStyles.fadeInLeft}`,
-  ])
-
+const About: React.FC = () => {
   return (
     <>
       <Meta
@@ -31,11 +23,9 @@ export default function About() {
         imageSrc="./images/about.jpg"
       />
       <Container>
-        <div className={styles.profile}>
-          <h3 className={useScrollAnimationStyles.fadeInUp}>
-            このサイトについて
-          </h3>
-          <p className={useScrollAnimationStyles.fadeInUp}>
+        <div className={styles['profile']}>
+          <h3>このサイトについて</h3>
+          <p>
             このサイトでは、ReactとNext.jsとmicroCMSを組み合わせ、プログラミングの技術ブログを制作しました。
             <br />
             ブログ記事では、学習過程で遭遇した具体的な課題や、解決策などを紹介しています。また、他の開発者やプログラミング学習者との知識の共有となることを目指しています。
@@ -43,10 +33,10 @@ export default function About() {
             今後も様々な記事を追加し、より充実したコンテンツを提供していきます。
           </p>
 
-          <h3 className={useScrollAnimationStyles.fadeInUp}>プロフィール</h3>
+          <h3>プロフィール</h3>
           <figure>
             <Image
-              className={`${useScrollAnimationStyles.fadeInUp} ${styles.profileImage}`}
+              className={styles['profileImage']}
               src={kakuta0915}
               alt=""
               objectFit="contain"
@@ -54,15 +44,15 @@ export default function About() {
               placeholder="blur"
             />
           </figure>
-          <Social className={useScrollAnimationStyles.fadeInUp} />
+          <Social isFooterSocial={false} />
 
-          <h4 className={useScrollAnimationStyles.fadeInUp}>名前</h4>
-          <p className={useScrollAnimationStyles.fadeInUp}>
+          <h4>名前</h4>
+          <p>
             閲覧ありがとうございます。
             <br />
             当サイトの管理をしている角田(かくた)です。
             <a
-              className={styles.profileName}
+              className={styles['profileName']}
               href="https://twitter.com/_kakuta0915_"
               target="_blank"
               rel="noreferrer"
@@ -70,14 +60,12 @@ export default function About() {
               (@_kakuta0915_)
             </a>
           </p>
-          <p className={useScrollAnimationStyles.fadeInUp}>
+          <p>
             技術力の向上と実践的なスキル構築を目指し、自主的にプログラミングを学習しています。これまでの学習過程やスキルセットについて、詳しくご紹介いたします。
           </p>
 
-          <h4 className={useScrollAnimationStyles.fadeInUp}>
-            エンジニアになりたいと思ったきっかけ
-          </h4>
-          <p className={useScrollAnimationStyles.fadeInUp}>
+          <h4>エンジニアになりたいと思ったきっかけ</h4>
+          <p>
             エンジニアになりたいと思ったきっかけは、IT技術を活用して社会課題に貢献できる仕事に就きたいと思ったからです。
             <br />
             私はこれまで介護施設や医療施設で働いてきましたが、この業界は人手不足や給与の低さをはじめ、さまざまな課題を抱えています。また、紙ベースの記録や手作業が多く、業務の効率化が進んでいないことが問題となっています。そのため、業務内容や長期的なキャリアに対して不満を感じるようになりました。
@@ -88,17 +76,15 @@ export default function About() {
             世の中には、業務の効率化や課題解決にIT技術を活かせる場面が多くあると感じています。こうした問題を解決し、社会に貢献できるエンジニアになりたいと考えています。
           </p>
 
-          <h4 className={useScrollAnimationStyles.fadeInUp}>技術スタック</h4>
-          <p className={useScrollAnimationStyles.fadeInUp}>
+          <h4>技術スタック</h4>
+          <p>
             HTML5 / CSS3 / JavaScript / Sass / TypeScript / Node.js / jQuery /
             React / TailwindCSS / Next.js / Git / GitHub / Gulp / microCMS /
             Linux / Firebase / Docker / ESLint / Prettier / Jest / Vercel
           </p>
 
-          <h4 className={useScrollAnimationStyles.fadeInUp}>
-            これから学びたい技術
-          </h4>
-          <p className={useScrollAnimationStyles.fadeInUp}>
+          <h4>これから学びたい技術</h4>
+          <p>
             これまではフロントエンドのスキルを中心に積み重ねてきましたが、今後はバックエンドの技術にも焦点を当て、習得を進めていきたいと考えています。
             <br />
             バックエンドの技術を習得することで、データベースの設計やサーバーサイドの開発においても自身のスキルセットを強化できると考えています。特に、バックエンドの学習を通じてシステム全体の設計や機能の実装に深く関与できるようになり、フルスタックエンジニアとしてのステップアップを目指しています。
@@ -106,8 +92,8 @@ export default function About() {
             今後も新たな技術に積極的に挑戦し、幅広い視野を持ちながらスキルの向上を図っていきます。
           </p>
 
-          <h4 className={useScrollAnimationStyles.fadeInUp}>趣味</h4>
-          <p className={useScrollAnimationStyles.fadeInUp}>
+          <h4>趣味</h4>
+          <p>
             プログラミング, ゲーム, アニメ鑑賞, 筋力トレーニング, 絵描き, 読書
           </p>
         </div>
@@ -115,3 +101,5 @@ export default function About() {
     </>
   )
 }
+
+export default About
