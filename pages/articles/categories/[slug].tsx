709fc9e852920f82813d805e260bde049f5623d8
Takeru Kakuta <kakuta19960915@gmail.com>
2025-06-07 17:06:09 +0900

    types: [slug].tsxの型定義

--- pages/articles/categories/[slug].tsx
+++ pages/articles/categories/[slug].tsx
@@ -5,8 +5,50 @@ import PostHeader from '@/src/components/PostHeader'
 import Posts from '@/src/components/Posts'
 import CategoriesList from '@/src/components/CategoriesList'
 import Container from '@/src/components/Container'
+import { GetServerSidePropsContext } from 'next'
 
-export default function Category({ icon, name, posts, allCategories }) {
+type Icon = {
+  url: string
+  width: number
+  height: number
+}
+
+type Category = {
+  id: string
+  name: string
+  slug: string
+  icon: Icon
+}
+
+type PostType = {
+  id: string
+  title: string
+  slug: string
+  eyecatch: {
+    url: string
+    width: number
+    height: number
+  }
+  publishDate: string
+  categories: Category[]
+  source: string
+  likesCount: number
+  bookmarksCount: number
+}
+
+type CategoryProps = {
+  icon: Icon
+  name: string
+  posts: PostType[]
+  allCategories: Category[]
+}
+
+const Category: React.FC<CategoryProps> = ({
+  icon,
+  name,
+  posts,
+  allCategories,
+}) => {
   return (
     <>
       <Meta pageTitle={name} pageDesc={`${name}に関する記事`} />
@@ -16,25 +58,33 @@ export default function Category({ icon, name, posts, allCategories }) {
           title={` ${name}に関する記事`}
           subtitle="Blog Category"
         />
-        <Posts posts={posts} />
+        <Posts posts={posts} maxPosts={0} />
         <CategoriesList allCategories={allCategories} />
       </Container>
     </>
   )
 }
 
-export async function getServerSideProps(context) {
-  const categorySlug = context.params.slug
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const categorySlug = context.params?.['slug'] as string
+  if (!categorySlug) {
+    return { notFound: true }
+  }
   const allCategories = await getAllCategories()
-  const category = allCategories.find(({ slug }) => slug === categorySlug)
+  const category = allCategories.find(
+    ({ slug }: { slug: string }) => slug === categorySlug,
+  )
 
   if (!category) {
     return { notFound: true }
   }
 
-  let posts = []
+  let posts: PostType[] = []
   try {
-    posts = await getAllPostByCategory(category.id)
+    const fetchedPosts = await getAllPostByCategory(category.id)
+    posts = fetchedPosts ?? []
   } catch (error) {
     console.error('Error fetching posts:', error)
   }
