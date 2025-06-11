3a41011b0f0d4446d3862ff75b65c5388411a208
Takeru Kakuta <kakuta19960915@gmail.com>
2025-06-11 20:26:32 +0900

    ⚡️ types: _document.tsxに型定義

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+import { siteMeta } from '@/libs/constants'
+
+const { siteLang } = siteMeta || {}
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang={siteLang}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
