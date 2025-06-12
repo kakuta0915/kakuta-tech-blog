f66e7dea3326daff3439e440197f9eee2bdf2ffe
Takeru Kakuta <kakuta19960915@gmail.com>
2025-06-12 16:10:38 +0900

    test: PostHeaderのテストを修正

--- src/components/PostHeader/index.test.tsx
+++ src/components/PostHeader/index.test.tsx
@@ -1,45 +1,34 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import PostHeader from '.'
-import type { PostHeaderProps } from '.'
+import { mockPostHeader } from '@/src/__mocks__/post-header'
 import styles from './index.module.css'
 
-jest.mock('../convert/convertDate', () => ({
-  __esModule: true,
-  default: ({ dateISO }: { dateISO: string }) => <span>{dateISO}</span>,
-}))
-
 describe('PostHeader Component', () => {
-  const defaultProps: PostHeaderProps = {
-    icon: { url: '/icons/icon.png', width: 50, height: 50 },
-    title: 'Post Title',
-    subtitle: 'Post Subtitle',
-    publish: '2024-07-23T00:00:00Z',
-  }
-
-  it('アイコン画像をが正しくレンダリングされているか', () => {
-    render(<PostHeader {...defaultProps} />)
+  it('アイコン画像を正しくレンダリングできるか', () => {
+    render(<PostHeader {...mockPostHeader} />)
 
     const image = screen.getByAltText('')
-    expect(image).toHaveAttribute('src', defaultProps.icon.url)
-    expect(image).toHaveAttribute('width', defaultProps.icon.width.toString())
-    expect(image).toHaveAttribute('height', defaultProps.icon.height.toString())
+    expect(image).toHaveAttribute('src', mockPostHeader.icon.url)
+    expect(image).toHaveAttribute('width', mockPostHeader.icon.width.toString())
+    expect(image).toHaveAttribute(
+      'height',
+      mockPostHeader.icon.height.toString(),
+    )
   })
 
   it('タイトルとサブタイトルが正しく表示されるか', () => {
-    render(<PostHeader {...defaultProps} />)
+    render(<PostHeader {...mockPostHeader} />)
 
-    expect(screen.getByText(defaultProps.subtitle)).toBeInTheDocument()
-    expect(screen.getByText(defaultProps.title)).toBeInTheDocument()
+    expect(screen.getByText(mockPostHeader.subtitle)).toBeInTheDocument()
+    expect(screen.getByText(mockPostHeader.title)).toBeInTheDocument()
   })
 
   it('公開日が正しく表示されるか', () => {
-    render(<PostHeader {...defaultProps} />)
+    render(<PostHeader {...mockPostHeader} />)
 
-    if (defaultProps.publish) {
-      const publishDate = screen.getByText(defaultProps.publish)
-      expect(publishDate).toBeInTheDocument()
-    }
+    const publishDate = screen.getByText('2024年07月23日')
+    expect(publishDate).toBeInTheDocument()
 
     const clockIcon = screen.getByTestId('clock-icon')
     expect(clockIcon).toBeInTheDocument()
@@ -47,7 +36,7 @@ describe('PostHeader Component', () => {
 
   it('公開日が指定されていない場合はレンダリングされない', () => {
     const { container } = render(
-      <PostHeader {...defaultProps} publish={undefined} />,
+      <PostHeader {...mockPostHeader} publish={undefined} />,
     )
 
     expect(container.querySelector(`.${styles['publish']}`)).toBeNull()
