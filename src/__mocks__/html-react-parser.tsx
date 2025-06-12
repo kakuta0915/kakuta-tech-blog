287b45a5f06603821fee98521a3aefd05d41404e
Takeru Kakuta <kakuta19960915@gmail.com>
2025-06-12 14:58:01 +0900

    test: ConvertBodyコンポーネントのモックファイル作成

---
+++ src/__mocks__/html-react-parser.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MockParser = (html: string): React.ReactNode => {
+  if (html.includes('<img')) {
+    return <img src="/image.jpg" alt="Example image" width={600} height={400} />
+  }
+  if (html.includes('<code')) {
+    return (
+      <code className="hljs">
+        <span className="hljs-keyword">const</span>
+      </code>
+    )
+  }
+  if (html.includes('<p')) {
+    return <p>This is a paragraph.</p>
+  }
+  return html
+}
+
+export default MockParser
