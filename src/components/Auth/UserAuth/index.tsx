124dd71482759b5b03fdf7cc89051dbbcc4cbbd9
Takeru Kakuta <kakuta19960915@gmail.com>
2025-06-20 20:18:28 +0900

    type: UserAuthの型修正

--- src/components/Auth/UserAuth/index.tsx
+++ src/components/Auth/UserAuth/index.tsx
@@ -6,13 +6,23 @@ import 'react-toastify/dist/ReactToastify.css'
 import styles from './index.module.css'
 import UserInfo from '../UserInfo'
 import SignInWithGoogle from '../SignInWithGoogle'
+import { SimpleUserProps } from '@/types'
 
 const UserAuth: React.FC = () => {
   const [user] = useAuthState(auth)
 
+  // user が null でなければ SimpleUserProps 型に変換
+  const simpleUser: SimpleUserProps | null = user
+    ? {
+        uid: user.uid,
+        displayName: user.displayName ?? '',
+        photoURL: user.photoURL ?? '',
+      }
+    : null
+
   return (
     <div className={styles['authService']}>
-      {user ? <UserInfo user={user} /> : <SignInWithGoogle />}
+      {simpleUser ? <UserInfo user={simpleUser} /> : <SignInWithGoogle />}
       <ToastContainer
         position="top-center"
         autoClose={1500}
