5f9441c89a9e2200c278f5fb5005379a16e0d44f
Takeru Kakuta <kakuta19960915@gmail.com>
2025-07-12 18:58:38 +0900

    fix: TableOfContentsコンポーネントをfeaturesディレクトリに移動

--- features/articles/components/index.ts
+++ features/articles/components/index.ts
@@ -3,5 +3,17 @@ import Comments from './Comments'
 import Pagination from './Pagination'
 import PostBody from './PostBody'
 import PostCategories from './PostCategories'
+import PostHeader from './PostHeader'
+import SocialActions from './PostActions'
+import TableOfContents from './TableOfContents'
 
-export { CategoriesList, Comments, Pagination, PostBody, PostCategories }
+export {
+  CategoriesList,
+  Comments,
+  Pagination,
+  PostBody,
+  PostCategories,
+  PostHeader,
+  SocialActions,
+  TableOfContents,
+}
