daaf844fc5aa8644b5b0cde7eea1ce8b6e1c4192
Takeru Kakuta <kakuta19960915@gmail.com>
2025-07-12 18:19:51 +0900

    fix: PostBodyをfeaturesディレクトリに移動

--- features/articles/components/index.ts
+++ features/articles/components/index.ts
@@ -1,5 +1,6 @@
 import CategoriesList from './CategoriesList'
 import Comments from './Comments'
 import Pagination from './Pagination'
+import PostBody from './PostBody'
 
-export { CategoriesList, Comments, Pagination }
+export { CategoriesList, Comments, Pagination, PostBody }
