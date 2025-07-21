9a02643b2cecaa0289e4de0cd0cad9a09ccdf6d7
Takeru Kakuta <kakuta19960915@gmail.com>
2025-07-21 20:35:38 +0900

    fix: 遷移先の修正

--- app/my-account/MyAccount.tsx
+++ app/my-account/MyAccount.tsx
@@ -98,7 +98,7 @@ const MyAccount: React.FC = () => {
             height={80}
           />
           <p>{user.displayName}</p>
-          <Link href="/settings/account/edit" className={styles['edit']}>
+          <Link href="/settings/edit" className={styles['edit']}>
             編集
           </Link>
         </div>
