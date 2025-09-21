d13ccf0cb5644c3b0633b516205bb894456f653f
Takeru Kakuta <kakuta19960915@gmail.com>
2025-09-21 20:43:52 +0900

    fix: PostCategoriesコンポーネントのレイアウトを修正

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -125,7 +125,7 @@ export default async function ArticlePage({ params }: Props) {
               </Article.PostBody>
             </div>
             <div className={styles['sidebar']}>
-              <Article.PostActions postId={postId} title={title} />
+              {/* <Article.PostActions postId={postId} title={title} /> */}
               <Article.PostCategories categories={categories} />
               <div className={styles['tocDesktop']}>
                 <Article.TableOfContents toc={toc} />
