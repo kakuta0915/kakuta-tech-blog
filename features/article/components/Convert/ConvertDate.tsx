c0aaa250c3001e6ccfa42c5733a6103e6aa15a52
Takeru Kakuta <kakuta19960915@gmail.com>
2025-11-03 18:27:49 +0900

    types: ConvertDateのエラーを改善

--- features/article/components/Convert/ConvertDate.tsx
+++ features/article/components/Convert/ConvertDate.tsx
@@ -1,17 +1,24 @@
+'use client'
+
 import React from 'react'
 import { parseISO, format } from 'date-fns'
-import { ja } from 'date-fns/locale/ja'
-
-type ConvertDateProps = {
-  dateISO: string
-}
+import { ja } from 'date-fns/locale'
+import type { ConvertDateProps } from './types'
 
 const ConvertDate: React.FC<ConvertDateProps> = ({ dateISO }) => {
+  if (!dateISO) return null
+
+  let parsedDate: Date
+
+  try {
+    parsedDate = parseISO(dateISO)
+  } catch {
+    return <time>Invalid date</time>
+  }
+
   return (
     <time dateTime={dateISO}>
-      {format(parseISO(dateISO), 'yyyy年MM月dd日', {
-        locale: ja,
-      })}
+      {format(parsedDate, 'yyyy年MM月dd日', { locale: ja })}
     </time>
   )
 }
