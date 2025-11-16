14004b2c84fca0ce96c9b37ce4d9d518442005e1
Takeru Kakuta <kakuta19960915@gmail.com>
2025-11-16 21:46:40 +0900

    fix: Footerのテストを修正

--- components/Layouts/Footer/index.test.tsx
+++ components/Layouts/Footer/index.test.tsx
@@ -5,7 +5,6 @@ import '@testing-library/jest-dom'
 describe('Footer Component', () => {
   it('Socialコンポーネントがレンダリングされるか', () => {
     render(<Footer />)
-
     const socialElement = screen.getByTestId('social')
     expect(socialElement).toBeInTheDocument()
   })
