d49f2a01dc7c2682b6763220f9e5cf0662a1c9b9
Takeru Kakuta <kakuta19960915@gmail.com>
2025-11-18 19:58:11 +0900

    fix: PostCategoriesのテストを修正

--- features/article/components/PostCategories/index.test.tsx
+++ features/article/components/PostCategories/index.test.tsx
@@ -3,36 +3,49 @@ import { render, screen } from '@testing-library/react'
 import { mockCategories } from '@/__mocks__/categories'
 import PostCategories from '.'
 
-describe('PostCategories', () => {
-  it(' categoriesプロパティで渡したデータが正しく表示されるかを確認', () => {
-    render(<PostCategories categories={mockCategories} />)
+describe('PostCategories Component', () => {
+  it('categoriesプロパティで渡したデータが正しく表示されるかを確認', () => {
+    const categories = mockCategories.map((category) => ({
+      ...category,
+      icon: category.icon ?? { url: '', width: 24, height: 24 },
+    }))
 
-    expect(screen.getByText('カテゴリ')).toBeInTheDocument()
+    render(<PostCategories categories={categories} />)
 
-    mockCategories.forEach(({ name }) => {
+    categories.forEach(({ name }) => {
       expect(screen.getByText(name)).toBeInTheDocument()
     })
   })
 
   it('各カテゴリリンクが正しいパスに設定されているかを確認', () => {
-    render(<PostCategories categories={mockCategories} />)
+    const categories = mockCategories.map((category) => ({
+      ...category,
+      icon: category.icon ?? { url: '', width: 24, height: 24 },
+    }))
 
-    mockCategories.forEach(({ slug }) => {
+    render(<PostCategories categories={categories} />)
+
+    categories.forEach(({ slug }) => {
       const link = screen.getByRole('link', { name: new RegExp(slug, 'i') })
       expect(link).toHaveAttribute('href', `/articles/categories/${slug}`)
     })
   })
 
   it('各画像が正しい src、width、height、および alt 属性を持っているかを確認', () => {
-    render(<PostCategories categories={mockCategories} />)
+    const categories = mockCategories.map((category) => ({
+      ...category,
+      icon: category.icon ?? { url: '', width: 24, height: 24 },
+    }))
+
+    render(<PostCategories categories={categories} />)
 
     const images = screen.getAllByAltText('画像')
 
-    mockCategories.forEach(({ icon }, index) => {
+    categories.forEach(({ icon }, index) => {
       const image = images[index]
-      expect(image).toHaveAttribute('src', icon.url)
-      expect(image).toHaveAttribute('width', icon.width.toString())
-      expect(image).toHaveAttribute('height', icon.height.toString())
+      expect(image).toHaveAttribute('src', icon?.url)
+      expect(image).toHaveAttribute('width', icon?.width.toString())
+      expect(image).toHaveAttribute('height', icon?.height.toString())
     })
   })
 })
