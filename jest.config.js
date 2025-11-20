8686ab433c22d7354ed4fdd46405d7d7caa4440b
Takeru Kakuta <kakuta19960915@gmail.com>
2025-11-20 16:18:49 +0900

    fix: Jestの設定を修正

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts', 'dotenv/config'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.ts',
     '^@/(.*)$': '<rootDir>/$1',
     '^next/image$': '<rootDir>/__mocks__/next/image.tsx',
     '^next/link$': '<rootDir>/__mocks__/next/link.ts',
