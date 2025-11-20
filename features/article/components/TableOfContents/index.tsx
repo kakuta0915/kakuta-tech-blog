b26b3d3081eb3f5a22238a404e1125fac83d53a5
Takeru Kakuta <kakuta19960915@gmail.com>
2025-11-20 15:55:15 +0900

    update: TableOfContentsのcssをsassに更新

--- features/article/components/TableOfContents/index.tsx
+++ features/article/components/TableOfContents/index.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react'
 import { Link as Scroll } from 'react-scroll'
-import styles from './index.module.css'
+import styles from './index.module.scss'
 import { TableOfContentsProps } from './types'
 import { FontAwesomeIcon, faListUl } from '@/libs/icons'
 
